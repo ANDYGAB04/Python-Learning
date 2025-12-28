fc5b4175073646e2f9d491d2e7f778386e668a05
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2025-12-28 12:09:31 +0200

    Testing the principles of oop

--- OOP/TestPlace.py
+++ OOP/TestPlace.py
@@ -1,29 +1,31 @@
-# OOP
-class PlayerCharacter:
-    # Class Object Attribute
-    membership = True
+class User:
+    def sign_in(self):
+        print("login-in")
 
-    # Constructor
-    def __init__(self, name="anonymous", age=0):
-        self.name = name
-        self.age = age
-
-    # Methods
-    def shout(self):
-        print(f"My name is {self.name}")
-
-    # Class Method
-    @classmethod
-    def adding_things(cls, num1, num2):
-        return cls("Teddy", num1 + num2)
-
-    # Static Method
-    @staticmethod
-    def add(num1, num2):
-        return num1 + num2
+    def attack(self):
+        print("do nothing")
 
 
-player1 = PlayerCharacter.adding_things(2, 3)
+class Wizard(User):
+    def __init__(self, name, power):
+        self._name = name
+        self._power = power
+
+    def attack(self):
+        print(f"attacking with power of {self._power}")
 
 
-print(player1.age)
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self._name = name
+        self._num_arrows = num_arrows
+
+    def attack(self):
+        print(f"attacking with arrows {self._num_arrows}")
+
+
+wizard1 = Wizard("Merlin", 50)
+archer1 = Archer("Robin", 100)
+
+for char in [wizard1, archer1]:
+    char.attack()
